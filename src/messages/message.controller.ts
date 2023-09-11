36f4e5e08b70a4ee620f818b9db606c63cc0dc27
whyareyou <bionixxxd5@gmail.com>
2023-09-11 19:52:46 +0300

    add methods for delete and update message

--- src/messages/message.controller.ts
+++ src/messages/message.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Patch, Post } from '@nestjs/common';
 import { MessageRepository } from './message.service';
+import { Message } from './message.model';
 
 @Controller('messages')
 export class MessageController {
@@ -8,5 +9,13 @@ export class MessageController {
   @Post()
   getUserContactsAndLastMessages(@Body('id') id: string) {
     return this.messageService.getUserContactsAndLastMessages(id);
+  }
+  @Delete('/delete')
+  deleteMessage(@Body('id') id: string) {
+    return this.messageService.deleteMessage(id);
+  }
+  @Patch('/update')
+  updateMessage(@Body('message') message: Message) {
+    return this.messageService.updateMessage(message);
   }
 }
